30d91a90d7d158965b96883f1231af1c4e78b926
tsuellow <theo.suellow@gmail.com>
2021-05-01 19:22:42 -0600

    again

--- dist/GenericServer.js
+++ dist/GenericServer.js
@@ -61,6 +61,7 @@ var GenericServer = /** @class */ (function () {
         });
         wsServer.on("connection", function (ws) {
             ws.on("message", function (message) {
+                var e_1, _a, e_2, _b;
                 //type:0 message is in order to perform initial connect process
                 //type:1 message is in order to modify reception channels as well as transmitting own location
                 //type:2 message is in order to exclusively send own location
@@ -76,29 +77,53 @@ var GenericServer = /** @class */ (function () {
                             connectionList.set(jsonMsg.taxiId, conn);
                             var response = { type: 0, action: "SEND UDP" };
                             ws.send(JSON.stringify(response));
+                            console.log(response);
                             break;
                         case 1:
-                            // 2 processes:
-                            //1. publish location payload on redis for counterpart(drivers) to diseminate
-                            //2. find channel delta and tell redis add and remove connection from corresponding channels
-                            sendOwnLocationOut(utils_1.getSingleChannelName(jsonMsg.targeChannel, jsonMsg.city, targetType), jsonMsg.payloadCSV);
+                            try {
+                                // 2 processes:
+                                //1. publish location payload on redis for counterpart(drivers) to diseminate
+                                //2. find channel delta and tell redis add and remove connection from corresponding channels
+                                for (var _c = __values(jsonMsg.targetChannels), _d = _c.next(); !_d.done; _d = _c.next()) {
+                                    var channel = _d.value;
+                                    sendOwnLocationOut(utils_1.getSingleChannelName(channel, jsonMsg.city, targetType), jsonMsg.payloadCSV);
+                                }
+                            }
+                            catch (e_1_1) { e_1 = { error: e_1_1 }; }
+                            finally {
+                                try {
+                                    if (_d && !_d.done && (_a = _c.return)) _a.call(_c);
+                                }
+                                finally { if (e_1) throw e_1.error; }
+                            }
                             var existingConn = connectionList.get(jsonMsg.taxiId);
                             if (existingConn) {
                                 updateOwnChannels(existingConn, jsonMsg.receptionChannels);
                             }
                             break;
-                        default:
+                        default: try {
                             // publish location payload on redis for counterpart(drivers) to diseminate
-                            sendOwnLocationOut(utils_1.getSingleChannelName(jsonMsg.targeChannel, jsonMsg.city, targetType), jsonMsg.payloadCSV);
+                            for (var _e = __values(jsonMsg.targetChannels), _f = _e.next(); !_f.done; _f = _e.next()) {
+                                var channel = _f.value;
+                                sendOwnLocationOut(utils_1.getSingleChannelName(channel, jsonMsg.city, targetType), jsonMsg.payloadCSV);
+                            }
+                        }
+                        catch (e_2_1) { e_2 = { error: e_2_1 }; }
+                        finally {
+                            try {
+                                if (_f && !_f.done && (_b = _e.return)) _b.call(_e);
+                            }
+                            finally { if (e_2) throw e_2.error; }
+                        }
                     }
                 }
-                catch (_a) {
+                catch (_g) {
                     ws.send("illegal msg");
                 }
             });
             //when connection is ended the client is first removed from the delivery group
             ws.on("close", function (code, reason) {
-                var e_1, _a;
+                var e_3, _a;
                 try {
                     for (var connectionList_1 = __values(connectionList), connectionList_1_1 = connectionList_1.next(); !connectionList_1_1.done; connectionList_1_1 = connectionList_1.next()) {
                         var _b = __read(connectionList_1_1.value, 2), key = _b[0], value = _b[1];
@@ -111,12 +136,12 @@ var GenericServer = /** @class */ (function () {
                         }
                     }
                 }
-                catch (e_1_1) { e_1 = { error: e_1_1 }; }
+                catch (e_3_1) { e_3 = { error: e_3_1 }; }
                 finally {
                     try {
                         if (connectionList_1_1 && !connectionList_1_1.done && (_a = connectionList_1.return)) _a.call(connectionList_1);
                     }
-                    finally { if (e_1) throw e_1.error; }
+                    finally { if (e_3) throw e_3.error; }
                 }
             });
         });
@@ -132,6 +157,7 @@ var GenericServer = /** @class */ (function () {
                     //@ts-ignore
                     updateOwnChannels(connObj, [], true); //change all ip:port data to the newly arrived ip:port
                     var response = { type: 0, action: "SEND LOC" }; //in this step android needs to calculate its reception channels and send them
+                    console.log(response);
                     connObj === null || connObj === void 0 ? void 0 : connObj.ws.send(JSON.stringify(response));
                 }
                 catch (_a) {
@@ -153,7 +179,7 @@ var GenericServer = /** @class */ (function () {
         }
         //this function updates the channels tuned into
         function updateOwnChannels(connObj, newChannels, resetAll) {
-            var e_2, _a, e_3, _b, e_4, _c;
+            var e_4, _a, e_5, _b, e_6, _c;
             if (resetAll === void 0) { resetAll = false; }
             if (resetAll) {
                 //this is when the UDP IP or port changes while the ws connection persists
@@ -165,12 +191,12 @@ var GenericServer = /** @class */ (function () {
                         redisClient.hset(iterator, connObj.taxiId.toString(), connObj.dgramChannel);
                     }
                 }
-                catch (e_2_1) { e_2 = { error: e_2_1 }; }
+                catch (e_4_1) { e_4 = { error: e_4_1 }; }
                 finally {
                     try {
                         if (toModify_1_1 && !toModify_1_1.done && (_a = toModify_1.return)) _a.call(toModify_1);
                     }
-                    finally { if (e_2) throw e_2.error; }
+                    finally { if (e_4) throw e_4.error; }
                 }
             }
             else {
@@ -184,12 +210,12 @@ var GenericServer = /** @class */ (function () {
                         redisClient.hdel(iterator, connObj.taxiId.toString());
                     }
                 }
-                catch (e_3_1) { e_3 = { error: e_3_1 }; }
+                catch (e_5_1) { e_5 = { error: e_5_1 }; }
                 finally {
                     try {
                         if (toRemove_1_1 && !toRemove_1_1.done && (_b = toRemove_1.return)) _b.call(toRemove_1);
                     }
-                    finally { if (e_3) throw e_3.error; }
+                    finally { if (e_5) throw e_5.error; }
                 }
                 try {
                     for (var toAdd_1 = __values(toAdd), toAdd_1_1 = toAdd_1.next(); !toAdd_1_1.done; toAdd_1_1 = toAdd_1.next()) {
@@ -197,12 +223,12 @@ var GenericServer = /** @class */ (function () {
                         redisClient.hset(iterator, connObj.taxiId.toString(), connObj.dgramChannel);
                     }
                 }
-                catch (e_4_1) { e_4 = { error: e_4_1 }; }
+                catch (e_6_1) { e_6 = { error: e_6_1 }; }
                 finally {
                     try {
                         if (toAdd_1_1 && !toAdd_1_1.done && (_c = toAdd_1.return)) _c.call(toAdd_1);
                     }
-                    finally { if (e_4) throw e_4.error; }
+                    finally { if (e_6) throw e_6.error; }
                 }
             }
         }
