2c16cb3614ec7e30c3df51684c0949a9668d7f56
tsuellow <theo.suellow@gmail.com>
2021-05-02 16:45:56 -0600

    adding to security group

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,22 @@ import { GenericServer } from "./GenericServer";
 //add code to get ip address
 //add ip to ip pool on redis
 
-const driverServer: GenericServer = new GenericServer(
-  "driver",
-  "client",
-  3002,
-  12000,
-  "172.31.44.252"
-);
-
-driverServer.startServer();
-
 const clientServer: GenericServer = new GenericServer(
   "client",
   "driver",
-  3001,
+  3000,
   33333,
   "172.31.44.252"
 );
 
 clientServer.startServer();
+
+const driverServer: GenericServer = new GenericServer(
+  "driver",
+  "client",
+  4000,
+  44444,
+  "172.31.44.252"
+);
+
+driverServer.startServer();
